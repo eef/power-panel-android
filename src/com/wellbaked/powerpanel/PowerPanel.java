0b4c544fafe5e3979715abb7d8a0d30c0e7d681b
Arthur Canal <uberhaqer@gmail.com>
2011-02-22 02:01:40 +0000

    testing

--- src/com/wellbaked/powerpanel/PowerPanel.java
+++ src/com/wellbaked/powerpanel/PowerPanel.java
@@ -2,6 +2,7 @@ package com.wellbaked.powerpanel;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 
 public class PowerPanel extends Activity {
     
@@ -9,5 +10,12 @@ public class PowerPanel extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+    	// Doing some testing on super classes
+    	System.out.println("=======================");
+    	System.out.println(event.getDisplayLabel());
+    	return true;
     }
 }
