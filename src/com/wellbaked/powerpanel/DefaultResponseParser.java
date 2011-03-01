54f45575a5eca161e92869eccfdf0a3ed82629bd
Arthur <arthur.canal@googlemail.com>
2011-03-01 14:20:17 +0000

    removed comments

--- src/com/wellbaked/powerpanel/DefaultResponseParser.java
+++ src/com/wellbaked/powerpanel/DefaultResponseParser.java
@@ -1,20 +1,12 @@
 package com.wellbaked.powerpanel;
 
-import android.os.AsyncTask;
 import org.apache.http.HttpResponse;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-/**
- * Created by IntelliJ IDEA.
- * User: chris
- * Date: 01/03/11
- * Time: 11:18
- * To change this template use File | Settings | File Templates.
- */
-public class DefaultResponseParser implements ResponseParser {
+class DefaultResponseParser implements ResponseParser {
     public String parseResponse(HttpResponse response){
     String result = "";
     try{
--- src/com/wellbaked/powerpanel/Transport.java
+++ src/com/wellbaked/powerpanel/Transport.java
@@ -1,25 +1,10 @@
 package com.wellbaked.powerpanel;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
-import com.sun.security.sasl.ClientFactoryImpl;
 import org.apache.http.HttpResponse;
-import org.apache.http.HttpVersion;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.conn.scheme.PlainSocketFactory;
-import org.apache.http.conn.scheme.Scheme;
-import org.apache.http.conn.scheme.SchemeRegistry;
-import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpParams;
-import org.apache.http.params.HttpProtocolParams;
-
 import android.os.AsyncTask;
 
 public class Transport extends AsyncTask<String, Void, String> {
@@ -39,7 +24,7 @@ public class Transport extends AsyncTask<String, Void, String> {
 	@Override
 	protected String doInBackground(String... args) {
 		DefaultHttpClient client = clientFactory.buildClient();
-		String url = "http://192.168.0.100";
+		String url = "http://10.254.254.134";
 		HttpGet request = new HttpGet(url);
 		try {
 			HttpResponse response = client.execute(request);
